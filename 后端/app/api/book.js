fb3ed8cbb44d3c95c162452485dc39f16a9df7ec
jferLao <438127468@qq.com>
2020-02-15 11:24:11 +0800

    book commit

--- 后端/app/api/book.js
+++ 后端/app/api/book.js
@@ -3,13 +3,20 @@ const router = new Router({ prefix: '/book' })
 const { Auth } = require('../../middlewares/auth')
 const { success } = require('../lib/helper')
 const { HotBook } = require('../models/bookHot')
-
-// 获取热销图书的列表
+const { PositiveIntegerValidator } = require('../validators/validator')
+const { Book } = require('../models/book')
+    // 获取热销图书的列表
 router.post('/getHotBookList', async(ctx) => {
     const books = await HotBook.getAll()
     ctx.body = { books }
 })
 
+// 根据书本id获取书本详情
+router.post('/detail/:id', async ctx => {
+    const v = await new PositiveIntegerValidator().validate(ctx)
+        // const book = new Book()
+    ctx.body = await Book.detail(v.get('path.id'))
+})
 
 
 
--- 后端/app/models/book.js
+++ 后端/app/models/book.js
@@ -1,2 +1,36 @@
+const axios = require('axios')
+const util = require('util')
 const { Sequelize, Model } = require('sequelize')
 const { sequelize } = require('../../core/db')
+
+
+class Book extends Model {
+    constructor() {
+        super()
+    }
+
+    // 根据id获取书本详情
+    static async detail(id) {
+        const url = util.format(global.config.yushu.detailUrl, id) //拼接url地址
+        const detail = await axios.get(url) //发送请求
+        return detail.data //返回请求结果    
+    }
+}
+
+Book.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    fav_nums: {
+        type: Sequelize.INTEGER,
+        default: 0
+    }
+}, {
+    sequelize,
+    tableName: 'book'
+})
+
+module.exports = {
+    Book
+}
