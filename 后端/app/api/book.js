7eba9f00ef1446ca395f579c3063ec47edb148b0
jferLao <438127468@qq.com>
2020-02-16 16:26:00 +0800

    book api commit

--- 后端/app/api/book.js
+++ 后端/app/api/book.js
@@ -3,9 +3,10 @@ const router = new Router({ prefix: '/book' })
 const { Auth } = require('../../middlewares/auth')
 const { success } = require('../lib/helper')
 const { HotBook } = require('../models/bookHot')
-const { PositiveIntegerValidator, SearchValidator } = require('../validators/validator')
+const { PositiveIntegerValidator, SearchValidator, AddShortCommentValidator } = require('../validators/validator')
 const { Book } = require('../models/book')
 const { Favor } = require('../models/favor')
+const { Comment } = require('../models/bookComment')
 
 // 获取热销图书的列表
 router.post('/getHotBookList', async(ctx) => {
@@ -30,12 +31,12 @@ router.post('/search', async ctx => {
 
 // 获取图书的点赞数
 router.post('/favor/count', new Auth().m, async ctx => {
-    const count = await Book.getMyFavorBookCount(ctx.auth.uid)
-    ctx.body = {
-        count
-    }
-})
-
+        const count = await Book.getMyFavorBookCount(ctx.auth.uid)
+        ctx.body = {
+            count
+        }
+    })
+    //获取书本的点赞
 router.post('/favor/:bookId', new Auth().m, async ctx => {
     const v = await new PositiveIntegerValidator().validate(ctx, {
         id: 'bookId'
@@ -45,6 +46,42 @@ router.post('/favor/:bookId', new Auth().m, async ctx => {
     ctx.body = favor
 })
 
+// 添加短评
+router.post('/add/short_comment', new Auth().m, async ctx => {
+    const v = await new AddShortCommentValidator().validate(ctx, {
+        id: 'bookId'
+    })
+    await Comment.addComment(v.get('body.bookId'), v.get('body.content'))
+    success()
+})
+
+//根据id获取书籍的短评
+router.post('/short_comment/:bookId', new Auth().m, async ctx => {
+    const v = await new PositiveIntegerValidator().validate(ctx, {
+        id: 'bookId'
+    })
+    const bookId = v.get('path.bookId')
+    const comments = await Comment.getComment(bookId)
+    ctx.body = {
+        comments: comments,
+        bookId
+    }
+})
+
+// 获取热门关键字,直接固定词汇
+router.post('/hot_keyword', async ctx => {
+    ctx.body = {
+        'hot': ['Python',
+            '哈利·波特',
+            '村上春树',
+            '东野圭吾',
+            '白夜行',
+            '韩寒',
+            '金庸',
+            '王小波'
+        ]
+    }
+})
 
 
 module.exports = router
--- 后端/app/models/bookComment.js
+++ 后端/app/models/bookComment.js
@@ -2,11 +2,49 @@ const { Sequelize, Model } = require('sequelize')
 const { sequelize } = require('../../core/db')
 
 class Comment extends Model {
-
+    static async addComment(bookId, content) {
+            // 在数据库中根据书本id和短评内容查找
+            const comment = await Comment.findOne({
+                    where: {
+                        bookId,
+                        content
+                    }
+                })
+                // 如果短评不存在则创建一条,并且设置短评数为1,存在则数加一
+            if (!comment) {
+                return await Comment.create({
+                    bookId,
+                    content,
+                    nums: 1
+                })
+            } else {
+                // comment的nums加一
+                return await comment.increment('nums', {
+                    by: 1
+                })
+            }
+        }
+        // 根据书本id获取短评
+    static async getComment(bookId) {
+        const comments = await Comment.findAll({
+            where: {
+                bookId
+            }
+        })
+        return comments
+    }
 }
 
 Comment.init({
-
+    content: Sequelize.STRING(12),
+    nums: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    },
+    bookId: Sequelize.INTEGER,
+}, {
+    sequelize,
+    tableName: 'comment'
 })
 
 module.exports = {
