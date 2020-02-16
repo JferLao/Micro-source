87697787cf70de49c0e6119bb9e08d08bbb79ede
jferLao <438127468@qq.com>
2020-02-16 16:05:20 +0800

    commit

--- 后端/app/models/bookComment.js
+++ 后端/app/models/bookComment.js
@@ -1,2 +1,14 @@
 const { Sequelize, Model } = require('sequelize')
 const { sequelize } = require('../../core/db')
+
+class Comment extends Model {
+
+}
+
+Comment.init({
+
+})
+
+module.exports = {
+    Comment
+}
