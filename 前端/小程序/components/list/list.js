1cb27b128050b407da46955d3380c6f6baa3f96a
jferLao <438127468@qq.com>
2020-03-02 21:07:18 +0800

    mine commit

--- 前端/小程序/components/list/list.js
+++ 前端/小程序/components/list/list.js
@@ -1,32 +1,39 @@
 // components/list/list.js
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-        title: String,
-        link: String,
-        isItem_1: {
-            type: Boolean,
-            value: false
-        },
-        isItem_2: {
-            type: Boolean,
-            value: false
-        },
-        info:String
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+    link: String,
+    isItem_1: {
+      type: Boolean,
+      value: false
     },
-    /**
-     * 组件的初始数据
-     */
-    data: {
-
+    isItem_2: {
+      type: Boolean,
+      value: false
     },
+    isItem_3: {
+      type: Boolean,
+      value: false
+    },
+    info: String,
+    icon: String
+  },
+  attached: function () {
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
+  },
 
-    }
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
 })
