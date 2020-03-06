f16c9d07957dae1a0a5ad1081d6273b1f6592442
jferLao <438127468@qq.com>
2020-03-06 15:32:31 +0800

    commit

--- 前端/小程序/pages/sourceDetail/sourceDetail.js
+++ 前端/小程序/pages/sourceDetail/sourceDetail.js
@@ -1,32 +1,64 @@
-import { SourceModel } from '../../models/source'
+import {
+  SourceModel
+} from '../../models/source'
 const sourceModel = new SourceModel()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        source: ''
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sources: {},
+    video: '',
+    start: 1,
+    count: 5,
+    source: []
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        let id = options.sourceId
-        this.onGetSource(id)
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    let id = options.sourceId
+    this.onGetSource(id)
+    this.getLatestSource()
+  },
 
-    // 获取课程内容
-    onGetSource(id) {
-        sourceModel.getSourceById(id, (res) => {
-            let data = res.source.video
-            console.log(data);
-            this.setData({
-                source: data
-            })
-        })
+  // 获取课程内容
+  onGetSource(id) {
+    sourceModel.getSourceById(id, (res) => {
+      let data = res.source.video
+      this.setData({
+        video: data,
+        sources: res.source
+      })
+    })
+  },
+
+  // 查看课程
+  enterSource(event) {
+    if (event.detail.source.id) {
+      let id = event.detail.source.id
+      wx.navigateTo({
+        url: '../sourceDetail/sourceDetail?sourceId=' + id
+      })
     }
+  },
+  // 查看更多
+  onMore() {
+    wx.navigateTo({
+      url: '../soureMenu/sourceMenu'
+    })
+  },
+
+  // 获取最新课程
+  getLatestSource: function() {
+    sourceModel.getLatestSource(this.data.start, this.data.count, (res) => {
+      this.setData({
+        source: res.source
+      })
+    })
+  },
 
 
 })
